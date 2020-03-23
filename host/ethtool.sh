d0766c64fbe7269b730de8383f28078525bfb0d4
Andrew Klaus <andrew.klaus@cybera.ca>
2020-03-23 14:15:09 -0600

    Automatically setting max RX ring buffer size on boot

--- host/ethtool.sh
+++ host/ethtool.sh
@@ -2,4 +2,7 @@
 for int in eno1 enp101s0f0np0 enp101s0f1np1 enp179s0f0np0 enp179s0f1np1;
 do
         for i in rx tx sg tso ufo gso gro lro; do ethtool -K $int $i off; done
+
+        # Set max RX ring buffer size
+        ethtool -g $int |grep RX |head -1 |awk '{ print $2 }' | xargs /sbin/ethtool -G $int rx
 done
