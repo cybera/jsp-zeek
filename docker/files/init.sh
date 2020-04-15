24a491d12c64b01c6b438efab0e7bced76bf0d27
Andrew Klaus <andrew.klaus@cybera.ca>
2020-04-15 12:44:54 -0600

    Using @load for local.zeek.append file (if exists) instead

--- docker/files/init.sh
+++ docker/files/init.sh
@@ -3,9 +3,11 @@ set -ex
 INTERFACE=${1:br0}
 sed -i 's/^interface=.*/interface=af_packet::'"${INTERFACE}"'/g'  /opt/zeek/etc/node.cfg
 
+cp /opt/zeek/share/zeek/site/local.zeek.template /opt/zeek/share/zeek/site/local.zeek
+
 # Append to local.zeek
 if [ -f /opt/zeek/share/zeek/site/local.zeek.append ]; then
-  cat /opt/zeek/share/zeek/site/local.zeek.append >> /opt/zeek/share/zeek/site/local.zeek
+    echo "@load local.zeek.append" >> /opt/zeek/share/zeek/site/local.zeek
 fi
 
 /opt/zeek/bin/zeekctl install
